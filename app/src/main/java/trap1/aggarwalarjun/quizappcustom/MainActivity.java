346db09aac260cee9550737a04881bf1097d735f
Aggarwal <1498036@fcps.edu>
2019-09-09 09:13:29 -0400

    Initial commit

---
+++ app/src/main/java/trap1/aggarwalarjun/quizappcustom/MainActivity.java
@@ -0,0 +1,26 @@
+package trap1.aggarwalarjun.quizappcustom;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button enter = findViewById(R.id.button);
+
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newScreen = new Intent(getApplicationContext(),Main2Activity.class);
+                startActivity(newScreen);
+            }
+        });
+    }
+}
